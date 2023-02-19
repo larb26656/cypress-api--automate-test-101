ba7bf411c75d0f54919e72155b14c987fbfd9d21
Errortime <larb26656@gmail.com>
2023-02-19 20:42:55 +0700

    Feat (add login api test)

---
+++ cypress/e2e/auth/login.cy.ts
@@ -0,0 +1,35 @@
+describe('Login', () => {
+  it('Login_Success_CorrectUsernameAndPassword', () => {
+    // clear database
+    cy.request({
+      method: 'POST',
+      url: 'http://127.0.0.1:4000/api/dev/db/clear',
+    }).then(res => {
+      expect(res.status).equal(200);
+    });
+
+    //excute database
+    const initUserScript =
+      "INSERT INTO `user` (name,username,`password`, active, deleted) VALUES('user1', 'admin', '81dc9bdb52d04dc20036dbd8313ed055',1,0);";
+
+    cy.request({
+      method: 'POST',
+      url: 'http://127.0.0.1:4000/api/dev/db/sql/execute',
+      body: initUserScript,
+    }).then(res => {
+      expect(res.status).equal(200);
+    });
+
+    cy.request({
+      method: 'POST',
+      url: 'http://127.0.0.1:4000/auth/login',
+      failOnStatusCode: false,
+      body: {
+        username: 'admin',
+        password: '1234',
+      },
+    }).then(res => {
+      expect(res.status).equal(200);
+    });
+  });
+});
